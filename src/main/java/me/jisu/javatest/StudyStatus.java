924cd5f090e16679fcf6e3ef2773484cf4278326
jisu3316 <jisu3316@naver.com>
2022-07-21 16:00:36 +0900

    2. Assertion

---
+++ src/main/java/me/jisu/javatest/StudyStatus.java
@@ -0,0 +1,5 @@
+package me.jisu.javatest;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- src/test/java/me/jisu/javatest/StudyTest.java
+++ src/test/java/me/jisu/javatest/StudyTest.java
@@ -2,19 +2,41 @@ package me.jisu.javatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 만들기 ╯°□°）╯")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+
+//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+//        String message = exception.getMessage();
+//        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
+//        Study study = new Study(-10);
+//        assertNotNull(study);
+        //기대 값 왼쪽 나오는 값 오른 쪽이긴하나 상관없다.
+//        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
+//        assertEquals(StudyStatus.DRAFT, study.getStatus(),
+//                "스터디를 처음 만들면 상태값이 "+ StudyStatus.DRAFT + "여야 한다.");
+//                이런상태면 실패를 하든 성공을 하든 DRAFT연산을 하기 때문에 실패했을때만 연산을 하도록 람다식을 적용한다
+//        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.");
+
+//        assertAll(
+//                () -> assertNotNull(study),
+//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
+//                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.")
+//        );
     }
 
-    @DisplayName("테스트 메소드 \uD83D\uDe31")
+    @DisplayName("스터디 만들기 \uD83D\uDe31")
     @Test
     void create_new_study_again() {
         System.out.println("create1");
